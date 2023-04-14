ad6ff6e7b83c45786ae0d8dbadf8b1959006e9b1
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2023-04-14 11:49:41 +0300

    ==

--- js/sliders.js
+++ js/sliders.js
@@ -28,18 +28,7 @@ $(function () {
       $(".services-slider").slick("slickUnfilter");
     }
   });
-  let btnContainer = document.getElementById("service-filter");
-  let btns = btnContainer.getElementsByClassName("filter-btn");
-  for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-      let current = document.getElementsByClassName("current-filter");
-      current[0].className = current[0].className.replace(
-        " current-filter",
-        ""
-      );
-      this.className += " current-filter";
-    });
-  }
+
   $(".works").slick({
     slidesToShow: 2,
     slidesToScroll: 1,
@@ -54,8 +43,37 @@ $(function () {
       },
     ],
   });
-});
 
+  $(" .reviews-slider ").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    breakpoint: 768,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+});
+let btnContainer = document.getElementById("service-filter");
+let btns = btnContainer.getElementsByClassName("filter-btn");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("current-filter");
+    current[0].className = current[0].className.replace(" current-filter", "");
+    this.className += " current-filter";
+  });
+}
 // if ($(".services-swiper").length) {
 //   const swiper = new Swiper(".services-swiper", {
 //     loop: true,
